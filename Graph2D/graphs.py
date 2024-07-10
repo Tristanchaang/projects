a99bd70bd0ce0b703d4d6ca22482ac0e01740386
Tristanchaang <tristan.chaang@gmail.com>
2024-07-10 02:36:10 -0400

    trimmed whitespace

--- Graph2D/graphs.py
+++ Graph2D/graphs.py
@@ -1,6 +1,6 @@
-import numpy as np 
+import numpy as np
 import math, json, os
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import matplotlib.animation as anime
 import matplotlib.patches as mp
 from matplotlib import colors
@@ -26,7 +26,7 @@ put "flownetwork" into the loadfilename line above.
 
 When the figure is run, a live terminal will be running in the background.
 It will keep track the different inputs you type in or click on.
-At any point of time, to erase the current input (e.g. after clicking 
+At any point of time, to erase the current input (e.g. after clicking
 the wrong keys), press Esc to cancel.
 
 ADDING NODES:
@@ -49,9 +49,9 @@ ADDING EDGES:
     >> b=0.5,f=1,und (this creates an edge with weight 0, bend 0.5, flow 1, and undirected)
     >> w=1,f=1 (this creates an edge with weight 1, bend 0, flow 1, and directed by default)
     >> (nothing inputted: this creates a directed edge with no label, flow, nor bend)
-  then press Enter/Return. 
+  then press Enter/Return.
 ** Note **
-    (1) Do not use spaces! 
+    (1) Do not use spaces!
     (2) The order of the attributes doesn't matter
 
 MOVING NODES:
@@ -59,7 +59,7 @@ MOVING NODES:
 
 DELETING OBJECTS (nodes or edges):
 - Click the object, then Backspace. (OR: type "del" then Enter/Return.)
-** Note ** 
+** Note **
     (1) If you delete a node, all edges attached to it will also be deleted.
 
 HIGHLIGHTING OBJECTS:
@@ -71,7 +71,7 @@ TOGGLING FLOW:
 SAVE GRAPH:
 - Type a valid name (e.g. flownetwork), then click the Save Button.
 ** Note **
-    (1) They will be saved into a folder called saved_graphs. 
+    (1) They will be saved into a folder called saved_graphs.
     (2) This folder is created once you run this program for the first time!
 
 RUNNING A BFS/DFS/Dijkstra:
@@ -129,10 +129,10 @@ class node:
     def __init__(self,x,y,s):
 
         # very big scatter pt (circ)
-        self.shape = ax.scatter([x],[y], s=noderad*1000, ec="black", color=nodebg, linewidth=thickness,zorder=1) 
+        self.shape = ax.scatter([x],[y], s=noderad*1000, ec="black", color=nodebg, linewidth=thickness,zorder=1)
 
         self.labeltext = ax.text(x,y,s=s,horizontalalignment='center',verticalalignment='center', size=textsize,zorder=3)
-        
+
         self.mask = ax.text(x,y,s="",horizontalalignment='center',
                             verticalalignment='center', size=textsize, c="black", alpha=1, zorder = 2)
 
@@ -144,20 +144,20 @@ class node:
         self.xrange[1] = max(self.xrange[1], x+margin)
         self.yrange[0] = min(self.yrange[0], y-margin)
         self.yrange[1] = max(self.yrange[1], y+margin)
-        
+
         nodeset[(x,y)] = self
         adjmat[self] = []
 
     def highlight(self, boo, write=""):
         self.shape.set(ec="red" if boo else "black",
                        linewidth = 2 if boo else 1)
-        self.mask.set(text=write if boo else "", 
+        self.mask.set(text=write if boo else "",
                       x=self.coord[0],
                       y=self.coord[1] + 0.6 * noderad * (self.yrange[1]-self.yrange[0])/fig.get_figheight(),
                       c="red")
 
     def lowlight(self, boo, write="", color="black"):
-        self.mask.set(text=write if boo else "", 
+        self.mask.set(text=write if boo else "",
                       x=self.coord[0],
                       y=self.coord[1] + 0.6 * noderad * (self.yrange[1]-self.yrange[0])/fig.get_figheight(),
                       c=color)
@@ -175,7 +175,7 @@ class node:
 
     def __eq__(self, other):
         return self.coord == other.coord
-    
+
     def move(self, othercoord):
         del nodeset[self.coord]
         x2, y2 = othercoord
@@ -204,13 +204,13 @@ class edge:
         edgeset.setdefault((node1.coord,node2.coord), [])
         edgeset[node1.coord,node2.coord].append(self)
         adjmat[self.start].append((self.end, self))
-        if not arrow: 
+        if not arrow:
             adjmat[self.end].append((self.start, self))
 
     def dispflow(self, pos, boo):
         if boo:
             dist = 1/self.flowvalue if self.flowvalue!=0 else None
-            if dist: 
+            if dist:
                 self.flowdots.set_visible(True)
                 pts = points_on_line_(self.start.coord, self.end.coord, pos, dist, bend = self.bend)
                 self.flowdots.set_offsets(np.array(pts))
@@ -218,10 +218,10 @@ class edge:
                 self.flowdots.set_visible(False)
         else:
             self.flowdots.set_visible(False)
-    
+
     def highlight(self, boo):
         self.shape.set(color="red" if boo else "black", linewidth = 2 if boo else 1)
-    
+
     def changeflow(self, newvalue):
         self.flowvalue = newvalue
         self.labeltext.set(text=("" if self.flowvalue==0 else (str(self.flowvalue)+"/")) + str(self.weight))
@@ -235,7 +235,7 @@ class edge:
 
     def __repr__(self):
         return self.start.__repr__() + "-" + self.end.__repr__()
-    
+
     def __del__(self):
         self.shape.remove()
         self.flowdots.remove()
@@ -256,7 +256,7 @@ def arrow_(p,q,weight,bend,arrow, flow):
 
     arrowstyle = mp.ArrowStyle("->",head_length=6, head_width=3) if arrow else mp.ArrowStyle("-")
 
-    shape = mp.FancyArrowPatch((x1,y1),(x2,y2), 
+    shape = mp.FancyArrowPatch((x1,y1),(x2,y2),
                                 arrowstyle=arrowstyle,
                                 connectionstyle=mp.ConnectionStyle("arc3", rad=bend),
                                 shrinkA=noderad*20, shrinkB=noderad*20, zorder=0, linewidth=thickness)
@@ -264,8 +264,8 @@ def arrow_(p,q,weight,bend,arrow, flow):
 
     x3, y3 = bent_midpoint(p,q,bend)
 
-    if weight != "": 
-        labelbg = ax.scatter([x3],[y3], s=noderad*400, ec="none", color="white", linewidth=thickness,zorder=0) 
+    if weight != "":
+        labelbg = ax.scatter([x3],[y3], s=noderad*400, ec="none", color="white", linewidth=thickness,zorder=0)
     else: labelbg = None
 
     labeltext = ax.text(x3,y3,s=("" if flow==0 else (str(flow)+"/")) + str(weight),
@@ -318,7 +318,7 @@ mission = None
 def nextstep(_):
     global clickqueue
     try: new = next(mission)
-    except: 
+    except:
         for _,es in edgeset.items():
             for e in es:
                 e.highlight(False)
@@ -346,10 +346,10 @@ def savegraph(_):
     for _,es in edgeset.items():
         for e in es:
             for_json["edges"].append((e.start.coord, e.end.coord, e.weight, e.bend, e.arrow, e.flowvalue))
-    
+
     with open(relpath("saved_graphs/" + inputstatus + ".json"), "w") as f:
         json.dump(for_json,f)
-    
+
     print("Saved as " + inputstatus + ".json")
     inputstatus = ""
     print('>>', inputstatus)
@@ -372,7 +372,7 @@ def loadgraph(jsonname):
 def activatebutt(butt, numnodes):
 
     def f(func):
-        
+
         def activatealg(x):
 
             global clickqueue, mission
@@ -382,7 +382,7 @@ def activatebutt(butt, numnodes):
                 if numnodes == 2: print("Pick a source node and a terminal node!")
                 return
 
-            mission = func(adjmat, *[nodeset[clickqueue[u]] for u in range(-numnodes,0)]) 
+            mission = func(adjmat, *[nodeset[clickqueue[u]] for u in range(-numnodes,0)])
             clickqueue = []
             nextstep(x)
 
@@ -448,7 +448,7 @@ def dijkstra(graph, source):
 
     def extractmin(dictionary):
         return min(list(dictionary.items()), key=lambda x:x[1])[0]
-    
+
     fakedist = {node: float("inf") for node in graph}
     fakedist[source] = 0
 
@@ -456,18 +456,18 @@ def dijkstra(graph, source):
         n.lowlight(True, "$\\infty$" if fakedist[n] == float("inf") else str(fakedist[n]), "blue")
 
     truedist = {}
-    
+
     while fakedist:
         popped = extractmin(fakedist)
         poppeddist = fakedist[popped]
-        
+
         truedist[popped] = poppeddist
 
         for nb, e in graph[popped]:
             if nb in fakedist:
                 fakedist[nb] = min(fakedist[nb], poppeddist + (e.weight if e.weight!="" else 0))
-                nb.lowlight(True, 
-                            "$\\infty$" if fakedist[nb] == float("inf") else str(fakedist[nb]), 
+                nb.lowlight(True,
+                            "$\\infty$" if fakedist[nb] == float("inf") else str(fakedist[nb]),
                             "blue")
 
         foundedge = False
@@ -491,7 +491,7 @@ karpbutt = Button(plt.axes([0.40, 0, 0.15, 0.05]), "\\textbf{Edmonds-Karp}", ima
 
 @activatebutt(karpbutt,2)
 def edmondskarp(graph, source, terminal):
-    
+
     def augmentingpath(graph):
         resgraph = {v: [] for v in graph}
         for _,nbs in graph.items():
@@ -500,7 +500,7 @@ def edmondskarp(graph, source, terminal):
                     resgraph[e.end].append((e.start, e.flowvalue, e, "opp"))
                 if e.flowvalue < e.weight:
                     resgraph[e.start].append((e.end, e.weight-e.flowvalue, e, "par"))
-        
+
         visited = {source: (0,None,None,None)}
         levels = [{source}]
         cur_level = 0
@@ -513,7 +513,7 @@ def edmondskarp(graph, source, terminal):
                         visited[nb] = (cur_level+1, f,ed,d)
                         levels[cur_level+1].add(nb)
             cur_level+=1
-        
+
         if terminal not in visited: return None
         else:
             ans = []
@@ -535,7 +535,7 @@ def edmondskarp(graph, source, terminal):
         to_yield = set()
         for e,stat in ag[0]:
 
-            e.changeflow(e.flowvalue + (ag[1]) * (1 if stat == "par" else -1))   
+            e.changeflow(e.flowvalue + (ag[1]) * (1 if stat == "par" else -1))
 
             to_yield.add((e,))
             to_yield.add((e.start,))
@@ -547,9 +547,9 @@ def edmondskarp(graph, source, terminal):
 
             e.highlight(False)
             e.start.highlight(False)
-            e.end.highlight(False) 
+            e.end.highlight(False)
 
-        ag = augmentingpath(graph)  
+        ag = augmentingpath(graph)
 
 
 ##########################
@@ -566,7 +566,7 @@ defaultarrow = True # plot directed edges by default?
 def process_input():
     global clickqueue, nodeset, inputstatus, nodeset, edgeset, adjmat, defaultarrow
 
-    if len(clickqueue) == 0: # Click Queue: 
+    if len(clickqueue) == 0: # Click Queue:
         if inputstatus == "und":
             defaultarrow = False
             print("Default: Undirected edges")
@@ -601,7 +601,7 @@ def process_input():
                 print("Highlighted node")
 
     if len(clickqueue) == 2 and clickqueue[0] in nodeset and clickqueue[1] in nodeset: # Click Queue: coord0 coord1
-        
+
         coord0, coord1 = clickqueue
 
         if inputstatus == "hl":
@@ -617,13 +617,13 @@ def process_input():
                         nb.remove(ve)
                     if ve[1].start.coord == coord1 and ve[1].end.coord == coord0 and not ve[1].arrow:
                         nb.remove(ve)
-            
+
             if (coord0,coord1) in edgeset:
                 for e in edgeset[(coord0,coord1)]:
                     del e
 
                 del edgeset[(coord0,coord1)]
-            
+
             if (coord1, coord0) in edgeset:
                 edgeset[(coord1,coord0)] = [e for e in edgeset[(coord1,coord0)] if e.arrow]
 
@@ -663,7 +663,7 @@ def process_input():
         coord0, coord1 = clickqueue
 
         nodeset[coord0].move(coord1)
-        
+
         for edgecoord in edgeset.copy():
             if edgecoord[0] == coord0:
                 for e in edgeset[edgecoord]:
@@ -674,7 +674,7 @@ def process_input():
                         e.labelbg.set_offsets(np.array([[xm,ym]]))
                 edgeset[(coord1, edgecoord[1])] = edgeset[edgecoord]
                 del edgeset[edgecoord]
-                
+
             if edgecoord[1] == coord0:
                 for e in edgeset[edgecoord]:
                     e.shape.set_positions(edgecoord[0], coord1)
@@ -701,9 +701,9 @@ fig.canvas.mpl_connect('button_press_event', onclick)
 
 def onkey(event):
     global inputstatus, clickqueue, nodeset, mission
-    
+
     entered = event.key
-    
+
     toprint = True
 
     if entered == 'escape':
@@ -722,7 +722,7 @@ def onkey(event):
         inputstatus += '_' if entered=='`' else entered
     else:
         toprint = False
-    
+
     if toprint: print('>>', inputstatus)
     reshape_diagram()
 fig.canvas.mpl_connect('key_press_event', onkey)
@@ -743,7 +743,7 @@ if loadfilename: loadgraph(loadfilename)
 # Manual Build #
 ################
 
-'''Note: Objects built here may not be removeable (due to additional pointers), 
+'''Note: Objects built here may not be removeable (due to additional pointers),
 but you can save it and then reload it as a new graph, then they are removeable.'''
 
 # N,R = 15,10
@@ -763,6 +763,6 @@ but you can save it and then reload it as a new graph, then they are removeable.
 
 reshape_diagram()
 
-plt.show() 
+plt.show()
 fig.show()
 # ani.save(filename="graph.gif", writer="pillow")
